838fff47f73ba1de36718c67ab68d395c2594464
Rutvikbhambhi <bhambhirutvik2004@gmail.com>
2023-12-21 17:17:36 +0530

    java assignment

---
+++ demo/src/demo/custome_exception.java
@@ -0,0 +1,13 @@
+/*
+ * 2 - W.j.p. to use of custome Exception 
+ * */
+package demo;
+
+public class custome_exception {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
